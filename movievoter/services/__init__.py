3afff0dbc8ea316fc5254b0cdbb575163563b618
muhamedtj <55760734+muhamedtj@users.noreply.github.com>
2026-08-22 18:54:40 +0500

    Export core business rules

---
+++ movievoter/services/__init__.py
@@ -0,0 +1,4 @@
+from movievoter.services.interest_ratings import required_interest_ratings
+from movievoter.services.voting_rules import should_finish_poll_early
+
+__all__ = ["required_interest_ratings", "should_finish_poll_early"]
