adbd9dd3b91f0e8d89a930f5e2f3cdd96bd20195
muhamedtj <55760734+muhamedtj@users.noreply.github.com>
2026-08-22 18:54:00 +0500

    Export domain models

---
+++ movievoter/models/__init__.py
@@ -0,0 +1,11 @@
+from movievoter.models.enums import MovieStatus, ReportStatus, Stage
+from movievoter.models.movie import MovieCandidate, MovieDetails, MovieSearchResult
+
+__all__ = [
+    "MovieCandidate",
+    "MovieDetails",
+    "MovieSearchResult",
+    "MovieStatus",
+    "ReportStatus",
+    "Stage",
+]
